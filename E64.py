944c36051b43dc1005611db1cfccc0742cf706ea
Emma Liden <egnidedon@gmail.com>
2016-06-12 13:04:28 +0100

    Finishing Exercise 64

--- E64.py
+++ E64.py
@@ -15,23 +15,31 @@ while str(value) != '':
 
 	value = input()
 
-print("The total is %r" % (total))	
+print("The total is $%r" % (total))	
 
 total_pennies = total * 100
 
-remainder_nickels = total_pennies % 5
+#print("Total Pennies: %r" % (total_pennies))
 
-if remainder_nickels < 2.5:
+remainder_pennies = total_pennies % 5
 
-	integer_pennies = total_pennies - remainder_nickels
+#print("Remainder Nickels: %r" % (remainder_nickels))
+
+if remainder_pennies < 2.5:
+
+	integer_pennies = total_pennies - remainder_pennies
 
 else:
 
-	integer_pennies = total_pennies + 5 - remainder_nickels
+	integer_pennies = total_pennies + 5 - remainder_pennies
 
 integer_dollars = int(integer_pennies/100)
 
+integer_nickels = int((integer_pennies -100*integer_dollars)/5)
 
+total_cash = integer_dollars + integer_nickels*5/100
+
+print("In cash that is $%.02f" % (total_cash))
 
 
 
